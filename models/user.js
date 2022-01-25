49133d6a2b60b1937f48dd8efc5568a5883a56f8
“DevynMalone” <Dee_malone24@yahoo.com>
2022-01-25 13:41:30 -0800

    User signup and Auth completed

---
+++ models/user.js
@@ -0,0 +1,73 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+
+const itemSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    descirption: {
+        type: String,
+        required: true
+    },
+    photoUrl: {
+        type: String,
+        required: true
+    },
+    dateAdded: {
+        type : Date,
+        default: new Date()
+    }
+})
+
+const collectionSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        // required: true
+    },
+    category: {
+        type: String,
+        // required: true
+    },
+    about: {
+        type: String,
+        required: true
+    },
+    photoUrl: {
+        type: String,
+        requried: true
+    },
+    items: [itemSchema]
+
+})
+
+
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true // makes sure email is not the same 
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    profilePic: {
+        type: String,
+        required: true
+    },
+    collections: [collectionSchema]
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
